f16c9dca3dc4df51c7527c08c02dc3b6206dab7f
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-16 20:10:44 +0700

    Create User Resolver

---
+++ src/graphql/resolvers/UserResolver.ts
@@ -0,0 +1,93 @@
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import * as bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
+import { UserModel } from "../../models/user";
+import { LoginPayload } from "../";
+
+@Resolver(() => User)
+export default class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "Hello World!";
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    try {
+      const emailExist = await UserModel.findOne({ email });
+      if (emailExist) {
+        return Error(`Oops, email is already exist`);
+      }
+      const usernameExist = await UserModel.findOne({ username });
+      if (usernameExist) {
+        return Error(`Oops, username is already exist`);
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = await UserModel.create({
+        email,
+        username,
+        password: hashedPassword,
+        firstName,
+        lastName,
+      });
+      await user.save();
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Mutation(() => LoginPayload)
+  async loginWithUsername(
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ) {
+    try {
+      const user = await UserModel.findOne({ username });
+      if (!user) {
+        return Error(`Oops, there's no user with this username`);
+      }
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return Error(`Oops, password is incorrect`);
+      }
+      return {
+        user,
+        message: "Successfully login",
+        token: "asdakj12kj3h12j3vh12h3",
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Mutation(() => LoginPayload)
+  async loginWithEmail(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return Error(`Oops, there's no user with this email`);
+      }
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return Error(`Oops, password is incorrect`);
+      }
+      return {
+        user,
+        message: "Successfully login",
+        token: "asdakj12kj3h12j3vh12h3",
+      };
+    } catch (error) {
+      return error;
+    }
+  }
+}
