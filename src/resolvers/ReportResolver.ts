1e30b50398bcd0bf7ee0be5f18cc94aa24ac56d1
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-17 18:56:58 +0700

    Add CRUD

--- src/resolvers/ReportResolver.ts
+++ src/resolvers/ReportResolver.ts
@@ -1,14 +1,21 @@
 import { Report, ReportModel } from "../entity/Report";
 import { UserModel } from "../entity/User";
-import { Arg, FieldResolver, Mutation, Resolver, Root } from "type-graphql";
-import { CreateInput } from "../utils/@types";
+import {
+  Query,
+  Arg,
+  FieldResolver,
+  Mutation,
+  Resolver,
+  Root,
+} from "type-graphql";
+import { CreateInput, EditInput } from "../utils/@types";
 
 @Resolver(() => Report)
 export default class ReportResolver {
   @FieldResolver()
-  async reporter(@Root() parent: Report) {
-    if (!parent.reporterId) return null;
-    const user = await UserModel.findOne({ _id: parent.reporterId });
+  async reporter(@Root() { reporterId }: Report) {
+    if (!reporterId) return null;
+    const user = await UserModel.findOne({ _id: reporterId });
     return user;
   }
 
@@ -27,8 +34,57 @@ export default class ReportResolver {
         detail: { ...data },
       });
       await newReport.save();
-      console.log(newReport);
       return newReport;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Mutation(() => Report)
+  async deleteReport(@Arg("id") id: string) {
+    try {
+      const deleted = await ReportModel.findByIdAndDelete(id);
+      return deleted;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Mutation(() => Report)
+  async editReport(@Arg("id") id: string, @Arg("data") data: EditInput) {
+    const after = await ReportModel.findOneAndUpdate(
+      { _id: id },
+      { ...data },
+      {
+        new: true,
+      }
+    );
+    if (!after) {
+      return Error("Report with this id is not exist, please create one");
+    }
+    await after.save();
+    console.log("after saved: ", after);
+    const foundSaved = await ReportModel.findById(id);
+    console.log("foundSaved saved: ", foundSaved);
+    return after;
+  }
+
+  @Query(() => [Report])
+  async getAllReports() {
+    try {
+      const reports = await ReportModel.find();
+      return reports;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  @Query(() => Report)
+  async getReport(@Arg("id") id: string) {
+    try {
+      const report = await ReportModel.findById(id);
+      if (!report) return Error(`There's no report with this ${id}`);
+      return report;
     } catch (error) {
       return error;
     }
