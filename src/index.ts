6e360703969f153364455de7856999e15b337905
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-18 13:49:07 +0700

    Reorganize

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,24 @@
 import "reflect-metadata";
 import "dotenv/config";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloError, ApolloServer } from "apollo-server-express";
 import chalk from "chalk";
 import express from "express";
 import { connect } from "mongoose";
 import { buildSchema } from "type-graphql";
 import { resolvers } from "./resolvers";
-import { TypegooseMiddleware } from "./utils/typegoose-middleware";
-import { TypegooseEntityMiddleware } from "./utils/typegoose-entity-middleware";
+import { TypegooseMiddleware } from "./utils/middleware/typegoose-middleware";
+import { TypegooseEntityMiddleware } from "./utils/middleware/typegoose-entity-middleware";
+import { GraphQLError } from "graphql";
 
 (async () => {
-  const schema = await buildSchema({
-    resolvers,
-    globalMiddlewares: [TypegooseMiddleware, TypegooseEntityMiddleware],
-  });
-
-  const app = express();
   const PORT = process.env.PORT || 4040;
-  const server = new ApolloServer({ schema });
-
+  const app = express();
   app.use(express.json());
-
   await connect(process.env.MONGO_URI!, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
     .then(() => {
       console.log(chalk.yellow("[database] Mongo is connected"));
@@ -32,6 +26,25 @@ import { TypegooseEntityMiddleware } from "./utils/typegoose-entity-middleware";
     .catch((e) => {
       console.log(chalk.bgYellow("[database] Error: ", e.message));
     });
+
+  const schema = await buildSchema({
+    resolvers,
+    globalMiddlewares: [TypegooseMiddleware, TypegooseEntityMiddleware],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    /**
+     * Error Formatter
+     */
+    formatError: (e: GraphQLError) => {
+      if (e instanceof ApolloError) {
+        console.log("you should see me");
+      }
+      return e;
+    },
+    context: ({ req, res }) => ({ req, res }),
+  });
 
   app.get("/", (_, res) => {
     res.status(200).send("Yeay! Go to /graphql to have some fun!!");
