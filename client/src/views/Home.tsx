96697989813a8a9592268e33cb17b95f46007694
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-20 00:23:26 +0700

    Dynamically content

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,12 +1,32 @@
-import { Box, Heading } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Box, Heading, Text } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
+import useAuth from "../helpers/auth";
 
 export default function HomePage() {
+  const { isLoggedIn } = useAuth();
   return (
-    <Box>
-      <Heading textAlign="center">
-        Welcome! now you can go to <Link to="/dashboard"> dashboard</Link>
-      </Heading>
+    <Box
+      minH="60vh"
+      display="flex"
+      alignItems="center"
+      flexDir="column"
+      justifyContent="center"
+    >
+      <Heading textAlign="center">Welcome!</Heading>
+      {isLoggedIn ? (
+        <Text>
+          Now you can go to <Link to="/dashboard"> dashboard</Link>
+        </Text>
+      ) : (
+        <Text fontSize="24px">
+          Please{" "}
+          <Button fontSize="24px" variant="link">
+            <Link to="/login">login</Link>
+          </Button>{" "}
+          if you want to continue
+        </Text>
+      )}
     </Box>
   );
 }
