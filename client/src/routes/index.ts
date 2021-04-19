cb8acf4f98179b4cfb1a1ce2c9854bda49cdde43
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 23:52:51 +0700

    Add some pages

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -3,11 +3,22 @@ import Page404 from "../views/404Page";
 import HomePage from "../views/Home";
 import LoginPage from "../views/Login";
 import AllUsers from "../views/AllUsers";
+import Overview from "../views/Overview";
 
 export const routes = [
   {
     path: "/",
     component: HomePage,
+    exact: true,
+  },
+  {
+    path: "/login",
+    component: LoginPage,
+    exact: true,
+  },
+  {
+    path: "/dashboard",
+    component: Overview,
     exact: true,
   },
   {
@@ -18,11 +29,6 @@ export const routes = [
   {
     path: "/dashboard/users",
     component: AllUsers,
-    exact: true,
-  },
-  {
-    path: "/login",
-    component: LoginPage,
     exact: true,
   },
   {
