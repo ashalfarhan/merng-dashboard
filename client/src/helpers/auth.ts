9775a31a2878a1b851bb268de9f6c13bb54af729
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 22:11:25 +0700

    Modify helper auth

--- client/src/helpers/auth.ts
+++ client/src/helpers/auth.ts
@@ -1,10 +1,10 @@
 const auth = {
   isLoggedIn: localStorage.getItem("isLoggedIn")
     ? // @ts-ignore
-      JSON.parse(localStorage.getItem("isLoggedIn"))
+      Boolean(JSON.parse(localStorage.getItem("isLoggedIn")))
     : null,
   login() {
-    localStorage.setItem("isLoggedin", JSON.stringify(true));
+    localStorage.setItem("isLoggedIn", JSON.stringify(true));
   },
   logout() {
     localStorage.setItem("isLoggedIn", JSON.stringify(false));
