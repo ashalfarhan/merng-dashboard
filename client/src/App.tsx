aadc8937d92339f506ca4ac38aeef6ee65c85081
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 20:57:11 +0700

    implement routes for admin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import { routes } from "./routes";
+import PrivateRoute from "./views/Private";
+const adminRoutes = ["/admin", "/users"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
+      <Box
+        style={{
+          minHeight: "90vh",
+        }}
+        px="4"
+      >
+        <Grid templateColumns="repeat(4, 1fr)" rows={1} columnGap="4">
+          <GridItem>
+            <SideBar />
+          </GridItem>
+          <GridItem colSpan={3}>
+            <Switch>
+              {routes.map((route, idx) => {
+                adminRoutes.includes(route.path) ? (
+                  <PrivateRoute key={idx} {...route} />
+                ) : (
+                  <Route key={idx} {...route} />
+                );
+              })}
+            </Switch>
+            {/* <AllReports />
+            <AllUsers />
+            <Box>
+              <Heading>This is not All Report</Heading>
+            </Box> */}
+          </GridItem>
+        </Grid>
+      </Box>
+    </Box>
   );
 }
 
