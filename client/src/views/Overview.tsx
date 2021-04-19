dda66c8b94b4b0b0d6778bd15ca57e838fe53b4b
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 23:53:07 +0700

    Create new page

---
+++ client/src/views/Overview.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/layout";
+import Layout from "../components/Layout";
+
+export default function Overview() {
+  return (
+    <Layout>
+      <Box>
+        <Heading>Overview</Heading>
+      </Box>
+    </Layout>
+  );
+}
