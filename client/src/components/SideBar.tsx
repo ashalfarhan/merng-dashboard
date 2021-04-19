49b7fe51b7ac80a15e6d44704b7d4063e47b9b3b
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 22:11:10 +0700

    Remove unused

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,19 +1,10 @@
 import { Button } from "@chakra-ui/button";
 import { Box, VStack } from "@chakra-ui/layout";
-// import { useState } from "react";
 import { useHistory } from "react-router";
 
 export default function SideBar() {
-  const adminRoutes = ["/admin", "/users"];
-  // const [isAdmin, _] = useState(false);
-  const isAdmin = false;
   const history = useHistory();
   const handleNav = (e: string) => {
-    if (adminRoutes.includes(e) && isAdmin) {
-      console.log(true);
-      alert("You should be an admin!");
-      history.push("/");
-    }
     history.push(e);
   };
   return (
@@ -28,8 +19,6 @@ export default function SideBar() {
         <Button onClick={() => handleNav("/overview")}>Overview</Button>
         <Button onClick={() => handleNav("/reports")}>All Reports</Button>
         <Button onClick={() => handleNav("/users")}>All Users</Button>
-        {/* <Button onClick={() => setMenu(MENU.OVERVIEW)}>Overview</Button>
-        <Button onClick={() => setMenu(MENU.USERS)}>User</Button> */}
       </VStack>
     </Box>
   );
