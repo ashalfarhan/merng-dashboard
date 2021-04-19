b93d80fa55692d726562430e6c8f4c024d753c58
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 23:51:51 +0700

    Modify header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,15 +11,14 @@ import { IconButton } from "@chakra-ui/button";
 import { BiMenuAltRight } from "react-icons/bi";
 import { ColorModeSwitcher } from "./ColorModeSwitcer";
 import { Link, useHistory } from "react-router-dom";
-import auth from "../helpers/auth";
+import useAuth from "../helpers/auth";
 
 export default function Header() {
   const history = useHistory();
+  const { logout, isLoggedIn } = useAuth();
   const handleLogout = () => {
-    setTimeout(() => {
-      auth.logout();
-      history.push("/login");
-    }, 300);
+    logout();
+    history.push("/login");
   };
   return (
     <Box p="4" display="flex" justifyContent="space-between">
@@ -28,23 +27,31 @@ export default function Header() {
       </Heading>
       <HStack spacing="24px" alignItems="center">
         <ColorModeSwitcher />
-        <Avatar size="md" />
-        <Menu>
-          <MenuButton as="button">
-            <IconButton icon={<BiMenuAltRight />} aria-label="Menu Icon" />
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Account</MenuItem>
-            <MenuDivider />
-            {auth.isLoggedIn ? (
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            ) : (
-              <MenuItem>
-                <Link to="/login">Login</Link>
-              </MenuItem>
-            )}
-          </MenuList>
-        </Menu>
+        {
+          isLoggedIn && (
+            <>
+              <Avatar size="md" />
+              <Menu>
+                <MenuButton as="button">
+                  <IconButton
+                    icon={<BiMenuAltRight />}
+                    aria-label="Menu Icon"
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Account</MenuItem>
+                  <MenuDivider />
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </>
+          )
+          //  : (
+          //   <Button>
+          //     <Link to="/login">Login</Link>
+          //   </Button>
+          // )
+        }
       </HStack>
     </Box>
   );
