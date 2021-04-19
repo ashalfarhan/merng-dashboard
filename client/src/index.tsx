4623c70356518d13dd11953226b40b29f0d83d84
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 23:51:41 +0700

    Add force refresh

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter forceRefresh>
       <ChakraProvider theme={theme}>
         <ColorModeScript />
         <App />
