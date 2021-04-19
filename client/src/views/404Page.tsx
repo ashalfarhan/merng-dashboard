4e49ca430be607e229f72b4938ff1fcff3c21d2b
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-20 00:23:12 +0700

    Modify not found

--- client/src/views/404Page.tsx
+++ client/src/views/404Page.tsx
@@ -1,13 +1,18 @@
+import { Button } from "@chakra-ui/button";
 import { Box, Heading, Text } from "@chakra-ui/layout";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Page404() {
   const { pathname } = useLocation();
   return (
-    <Box>
+    <Box px="12" mt="8" minH="80vh">
       <Heading>Oops, there's no page for {pathname}</Heading>
       <Text>
-        Back to <Link to="/">Home</Link>
+        Back to{" "}
+        <Button variant="link">
+          {" "}
+          <Link to="/">Home</Link>
+        </Button>
       </Text>
     </Box>
   );
