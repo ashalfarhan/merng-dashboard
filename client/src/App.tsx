6a18df4f48586dee8a160d9d6e4d2734b6d3b2f6
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-19 22:10:05 +0700

    Add protected route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import SideBar from "./components/SideBar";
 import { routes } from "./routes";
 import PrivateRoute from "./views/Private";
+
 const adminRoutes = ["/admin", "/users"];
 
 function App() {
+  const isProtected = (e: string) => {
+    return adminRoutes.includes(e);
+  };
   return (
     <Box
       style={{
@@ -14,34 +17,15 @@ function App() {
       }}
     >
       <Header />
-      <Box
-        style={{
-          minHeight: "90vh",
-        }}
-        px="4"
-      >
-        <Grid templateColumns="repeat(4, 1fr)" rows={1} columnGap="4">
-          <GridItem>
-            <SideBar />
-          </GridItem>
-          <GridItem colSpan={3}>
-            <Switch>
-              {routes.map((route, idx) => {
-                adminRoutes.includes(route.path) ? (
-                  <PrivateRoute key={idx} {...route} />
-                ) : (
-                  <Route key={idx} {...route} />
-                );
-              })}
-            </Switch>
-            {/* <AllReports />
-            <AllUsers />
-            <Box>
-              <Heading>This is not All Report</Heading>
-            </Box> */}
-          </GridItem>
-        </Grid>
-      </Box>
+      <Switch>
+        {routes.map((route, idx) =>
+          isProtected(route.path) ? (
+            <PrivateRoute key={idx} {...route} />
+          ) : (
+            <Route key={idx} {...route} />
+          )
+        )}
+      </Switch>
     </Box>
   );
 }
