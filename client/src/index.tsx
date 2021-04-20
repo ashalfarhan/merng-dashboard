222715288d6cc585d1ecb8c7bded78cf104a9a0c
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-20 19:30:01 +0700

    Add some providers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ColorModeScript, ChakraProvider, theme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./libs/apollo";
+import LocaleProvider from "./libs/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter forceRefresh>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript />
-        <App />
-      </ChakraProvider>
+      <LocaleProvider>
+        <ApolloProvider client={client}>
+          <ChakraProvider theme={theme}>
+            <ColorModeScript />
+            <App />
+          </ChakraProvider>
+        </ApolloProvider>
+      </LocaleProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
@@ -20,4 +26,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
