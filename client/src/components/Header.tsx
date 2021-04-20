1ce7c3b6ad34734fb5400b870df04e89d2918917
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-20 19:29:30 +0700

    Lang switcher

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,13 +12,24 @@ import { BiMenuAltRight } from "react-icons/bi";
 import { ColorModeSwitcher } from "./ColorModeSwitcer";
 import { Link, useHistory } from "react-router-dom";
 import useAuth from "../helpers/auth";
-
+import { FormattedMessage } from "react-intl";
+import { useContext } from "react";
+import { AppLocale } from "../libs/i18n";
+import { LOCALES } from "../@types/enums";
 export default function Header() {
   const history = useHistory();
+  const { locale, dispatch } = useContext(AppLocale);
   const { logout, isLoggedIn } = useAuth();
   const handleLogout = () => {
     logout();
     history.push("/login");
+  };
+  const isEng = locale === LOCALES.EN;
+  const handleSwitchLang = () => {
+    dispatch!({
+      type: "SET",
+      payload: isEng ? LOCALES.ID : LOCALES.EN,
+    });
   };
   return (
     <Box p="4" display="flex" justifyContent="space-between">
@@ -27,31 +38,28 @@ export default function Header() {
       </Heading>
       <HStack spacing="24px" alignItems="center">
         <ColorModeSwitcher />
-        {
-          isLoggedIn && (
-            <>
-              <Avatar size="md" />
-              <Menu>
-                <MenuButton as="button">
-                  <IconButton
-                    icon={<BiMenuAltRight />}
-                    aria-label="Menu Icon"
-                  />
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Account</MenuItem>
-                  <MenuDivider />
-                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </MenuList>
-              </Menu>
-            </>
-          )
-          //  : (
-          //   <Button>
-          //     <Link to="/login">Login</Link>
-          //   </Button>
-          // )
-        }
+        {isLoggedIn && (
+          <>
+            <Avatar size="md" />
+            <Menu>
+              <MenuButton as="button">
+                <IconButton icon={<BiMenuAltRight />} aria-label="Menu Icon" />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>
+                  <FormattedMessage id="settings.account" />
+                </MenuItem>
+                <MenuItem onClick={() => handleSwitchLang()}>
+                  {locale === LOCALES.EN ? "Bahasa Indonesia" : "English"}
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem onClick={handleLogout}>
+                  <FormattedMessage id="settings.logout" />
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </>
+        )}
       </HStack>
     </Box>
   );
