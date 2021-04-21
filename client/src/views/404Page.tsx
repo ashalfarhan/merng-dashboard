8bfdb25ee9e05901d278debd8376da57b3c01e51
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-21 11:48:47 +0700

    Modify 404 page content

--- client/src/views/404Page.tsx
+++ client/src/views/404Page.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@chakra-ui/button";
 import { Box, Heading, Text } from "@chakra-ui/layout";
+import { FormattedMessage } from "react-intl";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Page404() {
   const { pathname } = useLocation();
   return (
     <Box px="12" mt="8" minH="80vh">
-      <Heading>Oops, there's no page for {pathname}</Heading>
+      <Heading>404 Not Found</Heading>
+      <Heading>
+        Oops, <FormattedMessage id="notFound.heading" /> {pathname}
+      </Heading>
       <Text>
-        Back to{" "}
+        <FormattedMessage id="notFound.content.main" />{" "}
         <Button variant="link">
           {" "}
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <FormattedMessage id="notFound.content.sub" />
+          </Link>
         </Button>
       </Text>
     </Box>
