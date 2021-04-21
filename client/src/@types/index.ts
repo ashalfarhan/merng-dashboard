50cf5d99e56420a051929d1f8e116c450abc7f87
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-21 11:51:30 +0700

    Add types

--- client/src/@types/index.ts
+++ client/src/@types/index.ts
@@ -1,11 +1,8 @@
-import React from "react";
-import { LOCALES } from "./enums";
-
-export type LocaleAction =
-  | { type: "SET"; payload: LOCALES }
-  | { type: "RESET"; payload: LOCALES };
-
-export type AppLocaleCtxType = {
-  locale: string | LOCALES;
-  dispatch?: React.Dispatch<LocaleAction>;
-};
+export interface EmailLogin {
+  email: string;
+  password: string;
+}
+export interface UsernameLogin {
+  username: string;
+  password: string;
+}
