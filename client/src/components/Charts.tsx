5405cd7cf4c54fa677e53732af9ec109fb1c99c8
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-21 16:42:05 +0700

    mock chart

--- client/src/components/Charts.tsx
+++ client/src/components/Charts.tsx
@@ -25,29 +25,36 @@ export default function Charts({ display }: { display: OVERVIEW }) {
         width={600}
         options={{
           maintainAspectRatio: false,
+          y: {
+            ticks: {
+              callback: (val: number) => {
+                console.log("ticks y: ", val);
+                return `$${val}`;
+              },
+            },
+          },
         }}
         data={{
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: [
+            "Jan",
+            "Feb",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "Sept",
+            "Oct",
+            "Nov",
+            "Desc",
+          ],
           datasets: [
             {
-              label: "# of Votes",
+              label: "Last 2021",
               data: data,
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                // "rgba(54, 162, 235, 0.2)",
-                // "rgba(255, 206, 86, 0.2)",
-                // "rgba(75, 192, 192, 0.2)",
-                // "rgba(153, 102, 255, 0.2)",
-                // "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                // "rgba(54, 162, 235, 1)",
-                // "rgba(255, 206, 86, 1)",
-                // "rgba(75, 192, 192, 1)",
-                // "rgba(153, 102, 255, 1)",
-                // "rgba(255, 159, 64, 1)",
-              ],
+              backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+              borderColor: ["rgba(255, 99, 132, 1)"],
             },
           ],
         }}
