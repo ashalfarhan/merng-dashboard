b1326ce5759b8b4708cfed8989f09ca0a094818d
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-21 14:20:49 +0700

    Add error state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import { routes } from "./routes";
 import PrivateRoute from "./components/Private";
-
+import { useDispatch, useSelector } from "./store";
+import { closeError, getError } from "./store/slices/error";
+import { CgDanger } from "react-icons/cg";
 const protectedRoutes = [
   "/dashboard/users",
   "/dashboard/inventory",
@@ -13,9 +25,15 @@ const protectedRoutes = [
   "/dashboard",
 ];
 
+const isProtected = (e: string) => {
+  return protectedRoutes.includes(e);
+};
 function App() {
-  const isProtected = (e: string) => {
-    return protectedRoutes.includes(e);
+  const { isError, message } = useSelector(getError);
+  const dispatch = useDispatch();
+  const handleShutError = () => {
+    dispatch(closeError());
+    // window.location.reload();
   };
   return (
     <Box
@@ -33,6 +51,23 @@ function App() {
           )
         )}
       </Switch>
+      <Modal onClose={handleShutError} isOpen={isError} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader display="flex" alignItems="center">
+            <CgDanger />
+            <Text ml="2" color="red.600">
+              {"Oops"}
+            </Text>
+          </ModalHeader>
+          <ModalBody>{message}</ModalBody>
+          <ModalFooter>
+            <Button w="full" variant="outline" onClick={handleShutError}>
+              {"Ok"}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
