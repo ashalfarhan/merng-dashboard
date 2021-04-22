e0f7f6aa1bbfeb19c14ea9d976fe5278330e6545
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-23 00:10:38 +0700

    Adjust spacing

--- client/src/components/Private.tsx
+++ client/src/components/Private.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from "react-router-dom";
 import { useSelector } from "../store";
 import { isAuth } from "../store/slices/auth";
+
 export default function PrivateRoute({ component: Component, ...rest }: any) {
   const isLoggedIn = useSelector(isAuth);
   return (
