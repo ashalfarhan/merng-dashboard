6ccbff5f22f947fd41a3bc15408ff510750e71a5
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-24 01:55:46 +0700

    constants

--- client/src/helpers/constants.ts
+++ client/src/helpers/constants.ts
@@ -1,2 +1,38 @@
+import { ReportType, StuffType } from "../generated/graphql";
+
 export const API_URL =
   "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+
+export const StuffOptions = [
+  {
+    label: "stuff.type.foodsLabel",
+    value: StuffType.Foods,
+  },
+  {
+    label: "stuff.type.othersLabel",
+    value: StuffType.Others,
+  },
+  {
+    label: "stuff.type.personalLabel",
+    value: StuffType.Personal,
+  },
+  {
+    label: "stuff.type.stationaryLabel",
+    value: StuffType.Stationary,
+  },
+];
+
+export const ReportOptions = [
+  {
+    label: "report.type.purchase",
+    value: ReportType.Purchase,
+  },
+  {
+    label: "report.type.sell",
+    value: ReportType.Sell,
+  },
+  {
+    label: "report.type.wholesale",
+    value: ReportType.Wholesale,
+  },
+];
