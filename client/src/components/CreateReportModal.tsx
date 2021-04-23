5070fa05d2b2b00f09eb7e16e25b662c22da1ce9
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-24 01:54:33 +0700

    add create report modal

---
+++ client/src/components/CreateReportModal.tsx
@@ -0,0 +1,29 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import CreateReportForm from "./CreateReportForm";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function CreateReportModal({ isOpen, onClose }: Props) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create Report</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <CreateReportForm onComplete={onClose} />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
