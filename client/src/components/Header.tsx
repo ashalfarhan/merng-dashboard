55c223538eccf6205b93913fda54ae6349b08248
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-24 01:55:01 +0700

    adjust header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,44 +12,73 @@ import { BiMenuAltRight } from "react-icons/bi";
 import { ColorModeSwitcher } from "./ColorModeSwitcer";
 import { Link, useHistory } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
-import { LOCALES } from "../@types/enums";
 import { useDispatch, useSelector } from "../store";
-import { isAuth, removeToken } from "../store/slices/auth";
-import { getLocale, switchLocale } from "../store/slices/locale";
+import { getUser, isAuth, removeToken } from "../store/slices/auth";
+import { Tooltip } from "@chakra-ui/tooltip";
+import { getLocale } from "../store/slices/locale";
+import { LOCALES } from "../@types/enums";
+import LangSwitcher from "./LangSwitcher";
 
 export default function Header() {
   const history = useHistory();
   const isLoggedIn = useSelector(isAuth);
+  const user = useSelector(getUser);
   const currentLocale = useSelector(getLocale);
+  const isEng = currentLocale === LOCALES.EN;
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(removeToken());
     history.push("/login");
   };
-  const isEng = currentLocale === LOCALES.EN;
-  const handleSwitchLang = () => {
-    dispatch(switchLocale(isEng ? LOCALES.ID : LOCALES.EN));
-  };
+
   return (
-    <Box p="4" display="flex" justifyContent="space-between">
+    <Box
+      p="4"
+      display="flex"
+      justifyContent="space-between"
+      borderBottom="2px"
+      borderColor="teal"
+      mb="4"
+    >
       <Heading>
         <Link to="/">Dashboard</Link>
       </Heading>
       <HStack spacing="24px" alignItems="center">
         <ColorModeSwitcher />
-        {isLoggedIn && (
+        <LangSwitcher />
+        {isLoggedIn && user && (
           <>
-            <Avatar size="md" />
+            <Tooltip
+              label={`${isEng ? "Logged in as" : "Masuk sebagai"} ${
+                user.email
+              }`}
+              fontSize="12px"
+              aria-label="Logged in as"
+            >
+              <Avatar size="md" />
+            </Tooltip>
             <Menu>
-              <MenuButton as="button">
-                <IconButton icon={<BiMenuAltRight />} aria-label="Menu Icon" />
-              </MenuButton>
+              <MenuButton as={IconButton} icon={<BiMenuAltRight />} />
               <MenuList>
                 <MenuItem>
-                  <FormattedMessage id="settings.account" />
+                  <Link
+                    to="/me"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    <FormattedMessage id="settings.account" />
+                  </Link>
                 </MenuItem>
-                <MenuItem onClick={() => handleSwitchLang()}>
-                  {isEng ? "Bahasa Indonesia" : "English"}
+                <MenuItem>
+                  <Link
+                    to="/dashboard"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    Dashboard
+                  </Link>
                 </MenuItem>
                 <MenuDivider />
                 <MenuItem onClick={handleLogout}>
