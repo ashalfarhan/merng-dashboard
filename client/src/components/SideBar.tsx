8018e84e37788c07edb839b84d7e39107812d637
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-24 01:55:25 +0700

    adjust new page and feature

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@chakra-ui/button";
 import { Box, VStack } from "@chakra-ui/layout";
+import { useDisclosure } from "@chakra-ui/react";
 import { FormattedMessage } from "react-intl";
 import { useHistory } from "react-router";
 import { useSelector } from "../store";
 import { getUser } from "../store/slices/auth";
+import CreateReportModal from "./CreateReportModal";
 
 export default function SideBar() {
   const history = useHistory();
   const user = useSelector(getUser);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const handleNav = (e: string) => {
     history.push(e);
   };
@@ -39,6 +42,10 @@ export default function SideBar() {
         </Button>
         <Button onClick={() => handleNav("/dashboard/inventory")}>
           <FormattedMessage id="menu.inventory" />
+        </Button>
+        <Button onClick={onOpen}>
+          <CreateReportModal isOpen={isOpen} onClose={onClose} />
+          <FormattedMessage id="menu.createReportLabel" />
         </Button>
       </VStack>
     </Box>
