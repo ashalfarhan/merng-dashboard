0b6d070359157308e649c549f862bf8bea3a15cd
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-25 13:00:09 +0700

    adjust error

--- src/utils/middleware/isAuth.ts
+++ src/utils/middleware/isAuth.ts
@@ -5,14 +5,14 @@ import { MyContext } from "../@types";
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   const bearer = context.req.headers["authorization"];
   if (!bearer) {
-    throw new Error("Please include your token to access this route");
+    return Error("Please include your token to access this route");
   }
   try {
     const token = bearer.split(" ")[1];
     const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
     context.payload = payload as any;
   } catch (error) {
-    throw new Error(error.message);
+    return Error(error.message);
   }
   return next();
 };
