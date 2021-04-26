25de99a360fa35ac05b0ef785548c1d50d02e4d1
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-26 08:26:56 +0700

    Done: and it works

--- client/src/views/Inventory.tsx
+++ client/src/views/Inventory.tsx
@@ -1,12 +1,69 @@
-import { Box } from "@chakra-ui/layout";
-import { Heading } from "@chakra-ui/react";
-import Layout from "../components/Layout";
+import { Box, Heading } from "@chakra-ui/layout";
+import {
+  Link,
+  Spinner,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import moment from "moment";
+import { useIntl } from "react-intl";
+import Layout from "../components/common/Layout";
+import { useGetInventoryQuery } from "../generated/graphql";
+import { useDispatch } from "../store";
+import { setError } from "../store/slices/error";
 
-export default function Inventory() {
+export default function Stock() {
+  const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
+  const { data, loading } = useGetInventoryQuery({
+    onError: (e) => {
+      dispatch(setError(e.message));
+    },
+  });
+
   return (
     <Layout>
       <Box>
-        <Heading>Inventory Page</Heading>
+        <Heading>{formatMessage({ id: "menu.inventory" })}</Heading>
+        <Box height="full" display="flex">
+          {!data || loading ? (
+            <Spinner m="auto" />
+          ) : (
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>No.</Th>
+                  <Th>{formatMessage({ id: "report.nameLabel" })}</Th>
+                  <Th>{"Amount"}</Th>
+                  <Th>{"Price"}</Th>
+                  <Th>{formatMessage({ id: "report.reportedOnLabel" })}</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.getInventory?.map((report, idx) => (
+                  <Tr key={report._id}>
+                    <Td>{idx + 1}</Td>
+                    <Td>
+                      <Link to={`/report/${report._id}`}>{report.name}</Link>
+                    </Td>
+                    <Td>{report.amount}</Td>
+                    <Td>{report.price}</Td>
+                    <Td>{moment(report.createdAt).format("ddd DD-MM-YYYY")}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+              <TableCaption>
+                {formatMessage({ id: "lastUpdateLabel" })}
+                {new Date().toLocaleString()}
+              </TableCaption>
+            </Table>
+          )}
+        </Box>
       </Box>
     </Layout>
   );
--- client/src/views/Stock.tsx
+++ client/src/views/Stock.tsx
@@ -1,15 +1,67 @@
-// import { FormattedMessage } from "react-intl";
 import { Box, Heading } from "@chakra-ui/layout";
-import Layout from "../components/Layout";
+import {
+  Link,
+  Spinner,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import moment from "moment";
+import { useIntl } from "react-intl";
+import Layout from "../components/common/Layout";
+import { useGetStockQuery } from "../generated/graphql";
+import { useDispatch } from "../store";
+import { setError } from "../store/slices/error";
 
 export default function Stock() {
+  const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
+  const { data, loading } = useGetStockQuery({
+    onError: (e) => {
+      dispatch(setError(e.message));
+    },
+  });
+
   return (
     <Layout>
       <Box>
-        <Heading>
-          {/* <FormattedMessage id="overview.heading" /> */}
-          Stock Page
-        </Heading>
+        <Heading>{formatMessage({ id: "menu.stock" })}</Heading>
+        <Box height="full" display="flex">
+          {!data || loading ? (
+            <Spinner m="auto" />
+          ) : (
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>No.</Th>
+                  <Th>{formatMessage({ id: "report.nameLabel" })}</Th>
+                  <Th>{formatMessage({ id: "report.reporterLabel" })}</Th>
+                  <Th>{formatMessage({ id: "report.reportedOnLabel" })}</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.getStock?.map((report, idx) => (
+                  <Tr key={report._id}>
+                    <Td>{idx + 1}</Td>
+                    <Td>
+                      <Link to={`/report/${report._id}`}>{report.name}</Link>
+                    </Td>
+                    <Td>{report.reporter.name}</Td>
+                    <Td>{moment(report.createdAt).format("ddd DD-MM-YYYY")}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+              <TableCaption>
+                {formatMessage({ id: "lastUpdateLabel" })}
+                {new Date().toLocaleString()}
+              </TableCaption>
+            </Table>
+          )}
+        </Box>
       </Box>
     </Layout>
   );
