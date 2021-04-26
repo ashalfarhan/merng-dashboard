66a7b30dc1a54e64cf157f7d5bc7b2fa2c8809dd
ashalfarhan <ashalfarhan8@gmail.com>
2021-04-27 02:34:01 +0700

    adjustment

--- client/src/helpers/getChartData.ts
+++ client/src/helpers/getChartData.ts
@@ -11,31 +11,39 @@ export const getChartData = ({
 
   if (type === "STUFF") {
     displayData = data
-      .map((item: any) => ({ ...item, createdAt: new Date(item.createdAt) }))
-      .sort(
-        (prev: any, next: any) =>
-          next.createdAt.getMonth() - prev.createdAt.getMonth()
-      )
-      .map((good: any) => good.amount)
-      .reduce((prev: any, curr: any) => prev + curr);
+      .map((item: any) => ({
+        ...item,
+        month: new Date(item.createdAt).getMonth(),
+      }))
+      .sort((prev: any, next: any) => next.month - prev.month)
+      .map((item: any) => ({
+        amount: item.amount,
+        spends: item.price,
+        createdAt:
+          new Date(item.createdAt).getMonth() +
+          "/" +
+          new Date(item.createdAt).getFullYear(),
+      }));
+    return { displayData };
+  } else {
+    displayData = data
+      .map((item: any) => ({
+        ...item,
+        month: new Date(item.createdAt).getMonth(),
+      }))
+      .sort((prev: any, next: any) => next.month - prev.month)
+      .map((item: any) => ({
+        amount: item.goods
+          .map((good: any) => good.amount)
+          .reduce((prev: any, curr: any) => prev + curr),
+        income: item.goods
+          .map((good: any) => good.price)
+          .reduce((prev: any, curr: any) => prev + curr),
+        createdAt:
+          new Date(item.createdAt).getMonth() +
+          "/" +
+          new Date(item.createdAt).getFullYear(),
+      }));
+    return { displayData };
   }
-  displayData = data
-    .map((item: any) => ({
-      ...item,
-      month: new Date(item.createdAt).getMonth(),
-    }))
-    .sort((prev: any, next: any) => next.month - prev.month)
-    .map((item: any) => ({
-      amount: item.goods
-        .map((good: any) => good.amount)
-        .reduce((prev: any, curr: any) => prev + curr),
-      income: item.goods
-        .map((good: any) => good.price)
-        .reduce((prev: any, curr: any) => prev + curr),
-      createdAt:
-        new Date(item.createdAt).getMonth() +
-        "/" +
-        new Date(item.createdAt).getFullYear(),
-    }));
-  return { displayData };
 };
